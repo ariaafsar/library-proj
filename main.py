c68e791d1843097a098bb2aef2a11c3cbde6218f
parsamhb82 <parsa1382mohebian@gmail.com>
2024-08-02 21:06:15 +0330

    main.py pt1

---
+++ main.py
@@ -0,0 +1,63 @@
+from library import Member, Librarian, Book
+
+print('welcome')
+
+librarian = Librarian()
+
+while(True):
+    inp = input('1 - add a book \n2 - add a member\n3 - add stock to a book\n4 - search for a book\n5 - check for given books\n 6- check bill for a single member')
+
+    if inp == '1':
+        book_name = input('enter the book name :')
+        genre = input('enter the genre: ')
+        author = input('enter the author: ')
+        initial_stock = int(input('enter the initial stock as a number : '))
+        librarian.add_book(book_name, author, genre, initial_stock)
+    elif inp == '2':
+        member_name = input('enter the member name : ')
+        librarian.add_member(member_name)
+
+    elif inp == '3':
+        added_stock = int(input('enter the added stock as a number : '))
+        book_name = input(' enter the books name')
+        librarian.change_book_stock(book_name, added_stock)
+
+    elif inp == '4':
+        search_inp = input('by what do you want to search?\n1 - search by name\n2- search by author\n3 - search by genre\n')
+        if search_inp == '1':
+            name_input = input('enter the name : ')
+            book = librarian.find_book_by_name(name_input)
+            if book == None:
+                print('no match found')
+            else :
+                book : Book
+                print(f"{book.name} stock : {book.stock_book}")
+
+        elif search_inp == '2':
+            author_name = input("enter the authors name : ")
+            this_author_books = librarian.find_books_by_author(author_name)
+            if this_author_books == []:
+                print('no match found') 
+            else:
+                for book_name in this_author_books:
+                    print(book_name)
+        elif search_inp == '3':
+            genre_name = input('enter the genre: ')
+            this_genre_books = librarian.find_books_by_genre(genre_name)
+            if this_genre_books == []:
+                print('no match found')
+            else:
+                 for genre_name in this_genre_books:
+                    print(book_name)
+        else :
+            print('wrong input')
+    elif inp == '5':
+        pass
+    elif inp == '6':
+        member_name = input('give the member name: ')
+        member = librarian.find_member_by_name(member_name)
+        member : Member
+        bill = member.bill
+        print(f"{member_name}  {bill}")
+    else :
+        print('wrong input')
