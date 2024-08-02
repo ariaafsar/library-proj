646666d8b95f83d09346be3218f00b063084e599
Fatememdn <mohammadianfateme86@gmail.com>
2024-08-02 19:41:10 +0330

    librarian pt.1

--- library.py
+++ library.py
@@ -4,16 +4,28 @@ class Librarian:
         books_list = [] # list of objects of books
     def change_book_stock(self, book_name, number):
         pass
-    def add_book(self,book_name, initial_stock):
-        pass
+    def add_book(self,book_name,author, genre, initial_stock):
+        new_book = Book(book_name,author, genre, initial_stock)
+        self.books_list.append(new_book)        
     def add_member(self, member_name) :
-        pass
+        new_member = Member(member_name)
+        self.members_list.append(new_member)
+
     def give_book_to_member(self, member_name):
         pass
     def find_book_by_name(self, book_name):
-        pass
+        for book in self.books_list :
+            book : Book
+            if book_name == book.book_name:
+                return book
+        return None
+    
     def find_member_by_name(self, member_name):
-        pass
+        for member in self.members_list:
+            member : Member
+            if member_name == member.name:
+                return member
+        return None
     def find_books_by_author(self, author_name):
         pass
     def find_books_by_genre(self, genre):
